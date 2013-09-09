f7bd0a44454272445c20c4887ab28c5c34ca5ee9
Gavin Shriver <gavin.shriver@gmail.com>
2013-09-09 10:01:21 -0700

    satisfied requirements for stringifyJSON. Passes test, should be made cleaner if possible

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,53 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+             
+     if(_.isArray(obj)){
+        var stringObj = "[";
+        _.each(obj,function(val){
+            if(_.isUndefined(val)){
+              stringObj = stringObj.concat(stringifyJSON(null), ',');
+            }
+            else{
+              stringObj = stringObj.concat(stringifyJSON(val), ',');
+            }
+        });
+
+        if(stringObj.slice(-1) === ','){
+          return (stringObj.slice(0,-1)).concat("]");
+        }
+        else
+          return stringObj.concat("]")
+     }
+     else if(_.isNull(obj)){
+        return "null";
+     }
+     else if (_.isUndefined(obj)){
+        return "undefined";
+     }
+     else if (_.isFunction(obj)){
+        return stringifyJSON(obj.apply());
+     }
+     else if(_.isObject(obj)) {
+     	var stringObj = "{";
+
+      _.each(obj, function(val,key){
+        if(stringifyJSON(val) !== 'undefined') {
+          stringObj = stringObj.concat('"', key.toString(), '":', stringifyJSON(val), ',');
+        }
+      });
+
+        if(stringObj.slice(-1) === ','){
+          return (stringObj.slice(0,-1)).concat("}");
+        }
+        else
+          return stringObj.concat("}")
+      }
+      else {
+        if(typeof obj === 'string'){
+          return ('"').concat(obj,'"');
+        }
+        else
+          return obj.toString();
+      }
 };
