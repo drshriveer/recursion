20636088f2b324c954757ba8077060e8c6e69c5a
Gavin Shriver <gavin.shriver@gmail.com>
2013-09-10 16:14:14 -0700

    BROKEN function getElementsByClassName using recursion. *unable to see more than one class when using ClassList*

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,40 @@
 
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
+
+  var resultList = [];
+
+  /****  TEST CODE
+
+  console.log(document.body.childNodes[1]); // <div id="HTMLReporter" class="jasmine_reporter showDetails">...</div>
+  var cl = document.body.childNodes[1].classList; 
+  console.log(cl);  // DOMTokenList
+                    // 0: "jasmine_reporter"
+                    // 1: "showDetails"
+                    // length: 2
+                    // __proto__: DOMTokenList
+
+  console.log(cl.length); // 1
+  console.log(cl.contains('jasmine_reporter')); // true
+  console.log(cl.contains('showDetails')); // false
+  console.log(cl[0]); // jasmine_reporter
+  console.log(cl[1]); // null
+  console.log(_.values(cl)); // ["jasmine_reporter", 1] 
+
+ */
+  var recursionSearch = function(currentNode){
+
+    if(currentNode.contains(className)){
+      resultList.push(currentNode);
+    }
+
+    _.each(currentNode.childNodes, function(node){
+      recursionSearch(node);
+    });
+  };
+
+  recursionSearch(document.body);
+
+  return resultList;
+
 };
