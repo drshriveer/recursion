847bc27d818918d5587dd5646351f9520f2a2ec4
Gavin Shriver <gavin.shriver@gmail.com>
2013-09-11 15:52:14 -0700

    Fixed classList test, everything working. removed test code.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,34 +7,20 @@
 var getElementsByClassName = function (className) {
 
   var resultList = [];
-
-  /****  TEST CODE
-
-  console.log(document.body.childNodes[1]); // <div id="HTMLReporter" class="jasmine_reporter showDetails">...</div>
-  var cl = document.body.childNodes[1].classList; 
-  console.log(cl);  // DOMTokenList
-                    // 0: "jasmine_reporter"
-                    // 1: "showDetails"
-                    // length: 2
-                    // __proto__: DOMTokenList
-
-  console.log(cl.length); // 1
-  console.log(cl.contains('jasmine_reporter')); // true
-  console.log(cl.contains('showDetails')); // false
-  console.log(cl[0]); // jasmine_reporter
-  console.log(cl[1]); // null
-  console.log(_.values(cl)); // ["jasmine_reporter", 1] 
-
- */
+ 
   var recursionSearch = function(currentNode){
 
-    if(currentNode.contains(className)){
-      resultList.push(currentNode);
+    var cl = currentNode.classList;
+    if(typeof cl !== 'undefined'){
+      if(cl.contains(className)){
+        resultList.push(currentNode);
+      }
     }
 
     _.each(currentNode.childNodes, function(node){
       recursionSearch(node);
     });
+    
   };
 
   recursionSearch(document.body);
