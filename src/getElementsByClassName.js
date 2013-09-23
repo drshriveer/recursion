a8bf4c50dfb0f8dee41373e3b1f26626aa0993b7
Hack Reactor Students <github@hackreactor.com>
2013-09-23 14:55:57 -0700

    cleared all for re-do in class

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,25 +6,4 @@
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
 
-  var resultList = [];
- 
-  var recursionSearch = function(currentNode){
-
-    var cl = currentNode.classList;
-    if(typeof cl !== 'undefined'){
-      if(cl.contains(className)){
-        resultList.push(currentNode);
-      }
-    }
-
-    _.each(currentNode.childNodes, function(node){
-      recursionSearch(node);
-    });
-    
-  };
-
-  recursionSearch(document.body);
-
-  return resultList;
-
 };
