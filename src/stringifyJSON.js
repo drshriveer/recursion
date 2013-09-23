ab9143e7a306672616e6b138d40ad66ed77ba286
Hack Reactor Students <github@hackreactor.com>
2013-09-23 15:16:16 -0700

    added all but functions -- the easy stuff

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,19 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-       
+  if(Array.isArray(obj)){
+    return "[" + _(obj).map(function (element){
+      return stringifyJSON(element);
+    }).join(',') + "]";
+  }else if(obj && typeof obj === "object") {
+    return "{" + _(obj).map(function (val, key){
+      return stringifyJSON(key) + ":" + stringifyJSON(val);
+    }).join(',') + "}";
+  }
+  else if(typeof obj === "string"){
+    return  '"' + obj + '"';
+  }
+  else{
+    return obj + "";
+  }
 };
